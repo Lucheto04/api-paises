d34fb6ebc0364027eb16794936c41aecab6131c0
Lucheto04 <124000317+Lucheto04@users.noreply.github.com>
2023-05-14 16:48:50 -0500

    Add files via upload

---
+++ js/darck-mode.js
@@ -0,0 +1,18 @@
+const btnDark = document.querySelector('.btn-dark-mode');
+
+btnDark.addEventListener('click', () => {
+    console.log('diste click');
+    document.body.classList.toggle('dark-mode');
+
+    if(document.body.className === 'dark-mode') {
+        btnDark.innerHTML = /*html*/ `
+        <i class="fa-regular fa-sun"></i>
+        Ligth Mode
+        `;
+    } else {
+        btnDark.innerHTML = /*html*/ `
+        <i class="fa-regular fa-moon"></i>
+        Dark Mode
+        `;
+    }
+});
